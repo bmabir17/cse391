e4e4411917bb8c47349a876b5c13d7e642f9bcf5
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-05-09 16:16:47 +0600

    Lab1 milestone1

---
+++ lab1/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>B M Abir</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<div>
+	<div class="main_menu">
+		<div class="col-md-4"><button class="btn">Home</button> </div>
+		<div class="col-md-4" class="btn"><button class="btn">Projects</button> </div>
+		<div class="col-md-4" class="btn"><button class="btn">Experince</button> </div>
+	</div>
+	<div>
+		<div class="col-md-3 "><img class="pro-pic" src="myPic.jpg"></div>
+		<div class="col-md-9">
+			<div>
+				<h3 class="h3-heading">WHO AM I</h3>
+				<hr>
+				<h1 class="h1-heading"> B M Abir</h1>
+				<h2 class="h2-heading">Web Developer</h2>
+				<div class="col-md-6">
+					<p class="p-describe">Focused and detail-oriented Software and Embedded System Engineer offering exceptional troubleshooting skills and a talent for developing innovative solutions to unusual and difficult problems. Developed several internal tools to automate mundane tasks.</p>
+					<div class="table-class">
+						<table border="1">
+							<tr>
+								<th>Name</th>
+								<td>B M Abir</td>
+							</tr>
+							<tr>
+								<th>Date of Birth</th>
+								<td>January 17, 1993</td>
+							</tr>
+							<tr>
+								<th>Address</th>
+								<td>32, Arambag, motijheel</td>
+							</tr>
+
+						</table>
+					</div>
+					
+				</div>
+				<div class="col-md-6">
+					<p class="p-describe">I like to solve problems, learn new technologies and can’t get my hands off new hardware tools. I like to integrate the best of both worlds. Implementing crazy, new and innovative project that comes right out of my head.</p>
+					<div class="table-class">
+						<table border="1">
+							<tr>
+								<th>Email</th>
+								<td>bmabir17@gmail.com</td>
+							</tr>
+							<tr>
+								<th>Phone</th>
+								<td>+8801915601505</td>
+							</tr>
+							<tr>
+								<th>Facebook</th>
+								<td>bmabir17</td>
+							</tr>
+
+						</table>
+					</div>
+				</div>
+				<div></div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
