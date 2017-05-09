9aa1c82f971d1b447ce774b49866656657eca3f3
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-05-10 04:02:44 +0600

    2nd MileStone

--- lab1/index.php
+++ lab1/index.php
@@ -3,16 +3,31 @@
 <head>
 	<title>B M Abir</title>
 	<link rel="stylesheet" type="text/css" href="index.css">
+	<script src="https://use.fontawesome.com/f6207181d2.js"></script> <!--only used to get icons -->
 </head>
 <body>
 <div>
+	<!--
 	<div class="main_menu">
-		<div class="col-md-4"><button class="btn">Home</button> </div>
-		<div class="col-md-4" class="btn"><button class="btn">Projects</button> </div>
-		<div class="col-md-4" class="btn"><button class="btn">Experince</button> </div>
+		<div class="col-md-6">
+			<div class="col-md-12"><h1>B M Abir</h1></div>
+		</div>
+		<div class="col-md-6">
+			<div class="col-md-3 "><button class="btn" >Home</button> </div>
+			<div class="col-md-3 "><button class="btn" >Projects</button> </div>
+			<div class="col-md-3 "><button class="btn" >Portfolio</button> </div>
+			<div class="col-md-3 "><button class="btn" >Blog</button> </div>
+		</div>
 	</div>
+	-->
 	<div>
-		<div class="col-md-3 "><img class="pro-pic" src="myPic.jpg"></div>
+		<div class="col-md-3 main_menu">
+			<img class="pro-pic " src="myPic.jpg">
+			<div ><button class="btn" ><i class="fa fa-home fa-2x" aria-hidden="true"> Home </i> </button> </div>
+			<div ><button class="btn" ><i class="fa fa-cog fa-2x" aria-hidden="true"> Projects </i></button> </div>
+			<div ><button class="btn" ><i class="fa fa-th fa-2x" aria-hidden="true"> Portfolio</i></button> </div>
+			<div ><button class="btn" ><i class="fa fa-rss fa-2x" aria-hidden="true"> Blog</i></button> </div>
+		</div>
 		<div class="col-md-9">
 			<div>
 				<h3 class="h3-heading">WHO AM I</h3>
@@ -22,7 +37,7 @@
 				<div class="col-md-6">
 					<p class="p-describe">Focused and detail-oriented Software and Embedded System Engineer offering exceptional troubleshooting skills and a talent for developing innovative solutions to unusual and difficult problems. Developed several internal tools to automate mundane tasks.</p>
 					<div class="table-class">
-						<table border="1">
+						<table border="0">
 							<tr>
 								<th>Name</th>
 								<td>B M Abir</td>
@@ -42,8 +57,9 @@
 				</div>
 				<div class="col-md-6">
 					<p class="p-describe">I like to solve problems, learn new technologies and can’t get my hands off new hardware tools. I like to integrate the best of both worlds. Implementing crazy, new and innovative project that comes right out of my head.</p>
+					<br>
 					<div class="table-class">
-						<table border="1">
+						<table border="0">
 							<tr>
 								<th>Email</th>
 								<td>bmabir17@gmail.com</td>
@@ -60,7 +76,18 @@
 						</table>
 					</div>
 				</div>
-				<div></div>
+				<div class="col-md-12">
+					<h3 class="h3-heading">Programming Skills</h3>
+					<hr>
+					<div class="col-md-12">
+						<h4 style="margin-left:20px">Python</h4>
+						<progress style="margin-left:20px" value="9.0" max="10.0">
+					</div>
+					<div class="col-md-12">
+						<h4 style="margin-left:20px">JS</h4>
+						<progress style="margin-left:20px" value="9.0" max="10.0">
+					</div>
+				</div>
 			</div>
 
 		</div>
