cde1c5deb14838c24f004929479d693b50fc3cbe
SURFACEPRO4\bmabi <bmabir17@gmail.com>
2017-05-12 18:28:25 +0600

    cv mileStone2

--- lab1/index.php
+++ lab1/index.php
@@ -3,10 +3,11 @@
 <head>
 	<title>B M Abir</title>
 	<link rel="stylesheet" type="text/css" href="index.css">
+	<script type="text/javascript" src="index.js"></script>
 	<script src="https://use.fontawesome.com/f6207181d2.js"></script> <!--only used to get icons -->
 </head>
-<body>
-<div>
+<body class="content-body">
+<div class="content-body" id="content-body">
 	<!--
 	<div class="main_menu">
 		<div class="col-md-6">
@@ -20,76 +21,203 @@
 		</div>
 	</div>
 	-->
-	<div>
+	<div class="content">
 		<div class="col-md-3 main_menu">
 			<img class="pro-pic " src="myPic.jpg">
-			<div ><button class="btn" ><i class="fa fa-home fa-2x" aria-hidden="true"> Home </i> </button> </div>
+			<div ><button class="btn" ><i class="fa fa-home fa-2x " aria-hidden="true"> Home </i> </button></div>
 			<div ><button class="btn" ><i class="fa fa-cog fa-2x" aria-hidden="true"> Projects </i></button> </div>
 			<div ><button class="btn" ><i class="fa fa-th fa-2x" aria-hidden="true"> Portfolio</i></button> </div>
-			<div ><button class="btn" ><i class="fa fa-rss fa-2x" aria-hidden="true"> Blog</i></button> </div>
+			<div ><button class="btn" ><i class="fa fa-rss-square fa-2x" aria-hidden="true"> Blog </i></button> </div>
+			<div class="social-btn-area">
+				<div >
+					<button class="btn-round-social">
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+					</button>
+					<button class="btn-round-social">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</button>
+					<button class="btn-round-social">
+						<i class="fa fa-commenting" aria-hidden="true"></i>
+					</button>
+				</div>
+				<div>2017 <i class="fa fa-copyright"> BlueSphereIt</i> </div>
+			</div>
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-9 right-content">
 			<div>
-				<h3 class="h3-heading">WHO AM I</h3>
+				<h3 class="h3-heading"><b>Who Am I</b></h3>
 				<hr>
 				<h1 class="h1-heading"> B M Abir</h1>
-				<h2 class="h2-heading">Web Developer</h2>
+				<h2 class="h2-heading"><i> Web Developer</i></h2>
 				<div class="col-md-6">
 					<p class="p-describe">Focused and detail-oriented Software and Embedded System Engineer offering exceptional troubleshooting skills and a talent for developing innovative solutions to unusual and difficult problems. Developed several internal tools to automate mundane tasks.</p>
-					<div class="table-class">
-						<table border="0">
-							<tr>
-								<th>Name</th>
-								<td>B M Abir</td>
-							</tr>
-							<tr>
-								<th>Date of Birth</th>
-								<td>January 17, 1993</td>
-							</tr>
-							<tr>
-								<th>Address</th>
-								<td>32, Arambag, motijheel</td>
-							</tr>
-
-						</table>
-					</div>
+					
 					
 				</div>
 				<div class="col-md-6">
 					<p class="p-describe">I like to solve problems, learn new technologies and can’t get my hands off new hardware tools. I like to integrate the best of both worlds. Implementing crazy, new and innovative project that comes right out of my head.</p>
 					<br>
-					<div class="table-class">
-						<table border="0">
-							<tr>
-								<th>Email</th>
-								<td>bmabir17@gmail.com</td>
-							</tr>
-							<tr>
-								<th>Phone</th>
-								<td>+8801915601505</td>
-							</tr>
-							<tr>
-								<th>Facebook</th>
-								<td>bmabir17</td>
-							</tr>
+					
+				</div>
+			</div>
+			<div>
+				<div class="col-md-12">
+					<div class="col-md-6">
+						<div class="table-class">
+							<table border="0">
+								<tr>
+									<th>Name</th>
+									<td>B M Abir</td>
+								</tr>
+								<tr>
+									<th>Date of Birth</th>
+									<td>January 17, 1993</td>
+								</tr>
+								<tr>
+									<th>Address</th>
+									<td>32, Arambag, motijheel</td>
+								</tr>
 
-						</table>
+							</table>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="table-class" style="padding-left:8%">
+							<table border="0">
+								<tr>
+									<th>Email</th>
+									<td>bmabir17@gmail.com</td>
+								</tr>
+								<tr>
+									<th>Phone</th>
+									<td>+8801915601505</td>
+								</tr>
+								<tr>
+									<th>Facebook</th>
+									<td>bmabir17</td>
+								</tr>
+
+							</table>
+						</div>
 					</div>
 				</div>
+			</div>
+			<div>
 				<div class="col-md-12">
-					<h3 class="h3-heading">Programming Skills</h3>
+					<h3 class="h3-heading h3-line-brk">Programming Skills</h3>
 					<hr>
 					<div class="col-md-12">
 						<h4 style="margin-left:20px">Python</h4>
 						<progress style="margin-left:20px" value="9.0" max="10.0">
 					</div>
 					<div class="col-md-12">
-						<h4 style="margin-left:20px">JS</h4>
+						<h4 style="margin-left:20px">JavaScript</h4>
 						<progress style="margin-left:20px" value="9.0" max="10.0">
+					</div>
+				</div>
+
+			</div>
+			<div>
+				<div class="col-md-12">
+					<h3 class="h3-heading h3-line-brk">Experience</h3>
+					<hr>
+					<div class="div-work">
+						<button class="btn-work"><i class="fa fa-briefcase"></i></button>
+					</div>
+					<div class="div-work">
+						<div>
+							<ul>
+								<li>
+									<h4><a class="a-link" target="_blank" href="http://www.robu-lab.org">Robotics Club of BRAC University</a> </h4>
+									<div class="col-md-12">
+										<div class="col-md-9">
+											<p class="p-exp-subTitle">President & Project Lead</p>
+										</div>
+										<div class="col-md-3">
+											<p class="p-exp-subTitle">2015 - 2017</p>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<p>
+											<ul>
+												<li>Co-ordinate between different Departments</li>
+												<li>Planning, organizing and executing event plans</li>
+												<li>Train members on the field of Robotics</li>
+												<li>Lead unique Research projects.</li>
+											</ul>
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
 
+
+			<div>
+				<div class="col-md-12">
+					<h3 class="h3-heading h3-line-brk">Education</h3>
+					<hr>
+					<div class="div-work">
+						<button class="btn-work"><i class="fa fa-book"></i></button>
+					</div>
+					<div class="div-work">
+						<div>
+							<ul>
+								<div class="col-md-12">
+									<li>
+										<h4><a class="a-link" target="_blank" href="http://www.bracu.ac.bd"> BRAC University</a></h4>
+										<div class="col-md-12">
+											<div class="col-md-9">
+												<p class="p-exp-subTitle">Computer Science and Engineering</p>
+											</div>
+											<div class="col-md-2">
+												<p class="p-exp-subTitle">2017</p>
+											</div>
+										</div>
+										<div class="col-md-12">
+											<p class="col-md-9">
+												Currently studying for undergraduate program in CSE. Took courses like introduction to database systems, Computer Architecture, <a href="https://piazza.com/">Programing for the internet</a>  and Artificial Intelligence.
+											</p>
+										</div>
+									</li>
+								</div>
+								<div class="col-md-12">
+									<li>
+										<h4><a class="a-link edu-title" target="_blank" href="#"> Wills Little Flower Higher Secondary School</a></h4>
+										<div class="col-md-12">
+											<div class="col-md-9">
+												<p class="p-exp-subTitle">O Levels & A Levels</p>
+											</div>
+											<div class="col-md-2">
+												<p class="p-exp-subTitle">2010 2012</p>
+											</div>
+										</div>
+										<div class="col-md-12">
+											<p class="col-md-9">
+												Completed my O-levels with 6 A's and A-levels with 2 A's and 1 B's grade. My Core Subjects were Maths and Science. 
+											</p>
+										</div>
+									</li>
+								</div>
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+		<div class="col-md-12">
+			<div class="col-md-7">
+				<p id="credit">Powered By BlueShpereIt</p>
+			</div>
+			<div class="col-md-5">
+				<p id="showModDate"></p>
+			</div>	
+				
 		</div>
 	</div>
 </div>
